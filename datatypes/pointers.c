8e0cab2d2e2cc6f41f4f97176ed50ffb242c5ba4
Theo Reichert <theo.reichert7@gmail.com>
2020-12-08 23:55:33 +0100

    08.12.2020

--- datatypes/pointers.c
+++ datatypes/pointers.c
@@ -4,7 +4,13 @@
 
 int a = 20; /*the varible type is int (integer)*/
 int* b = &a; /*the variable type is int* meaning it is a pointer to an integer, not an integer which would be typedefined by simply int*/
-int advanced();
+int string_recovery(); // Declared but not defined/initialised. But since it is declared it can be called although it isnt defined/initialised.
+
+void pause() {
+    printf ( "Press [Enter] to continue..." );
+    fflush ( stdout );
+    getchar();
+}
 
 int give_a() {
     return a;
@@ -22,25 +28,32 @@ int give_value_pointer_b_points_at() {
     return *b;
 };
 
-int main() {
+void pointer_demo() {
+    printf("START:\t'pointer_demo'");
     printf("value(a)\tmemory address(a)\tmemory address(b)\tvalue(b)\n");
     printf("%d\t\t%x\t\t\t%x\t\t\t%d\n", give_a(), give_pointer_a(), give_pointer_b(), give_value_pointer_b_points_at());
-    printf("End\n");
-    system("PAUSE");
-    advanced();
-    system("PAUSE");
+    printf("END:\t'pointer_demo'");
+}
+
+int main() {
+    printf("START:\t'main'");
+    pause();
+    string_recovery();
+    printf("END:\t'main'");
+    pause();
     return 0;
 };
 
-int advanced() {
-    char string[10]; /* array/list of characters with space for 10 charcters*/
-    char* pointerarray[10]; /* array/list of pointers to characters with space for 10 of such*/
-    char text[10]; /* array/list of characters with space for 10 charcters*/
+void string_recovery() {
+    printf("START:\t'string_recovery'");
+    char string[10]; /* ( array | list ) of characters with space for 10 charcters*/
+    char* pointerarray[10]; /* ( array | list ) of ( memory address | pointer )s to characters with space for 10 of such*/
+    char text[10]; /* ( array | list ) of characters with space for 10 charcters*/
     printf("Type sth. 10 characters long: ");
     scanf("%s", string);
     printf("String you typed (red via 'string'):\t%s\n", string);
     printf("Memory address (red via '&string[0]') is:\t%x\n", &string[0]);
-    char* pointer_on_string = &string[0];
+    char* pointer_on_string = &string[0]; //( memory address | pointer ) to the first memory address holding ( a part | first element | first character ) of string[0]
     printf("Memory address is (red via variable 'pointer_on_string'):\t%x\n", pointer_on_string);
     /**/
     for (int x = 0; x < 10; x++) {
@@ -57,8 +70,5 @@ int advanced() {
     };
     printf("You typed (pointer resolution):\t%s\n", text);
     /**/
-    int integer;
-    scanf("%i", &integer);
-    system("PAUSE");
-    return 0;
+    printf("END:\t'string_recovery'");
 };
