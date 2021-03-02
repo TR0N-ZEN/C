ceb88c64889182cde5c56cf9b222e3167119ff14
= <=>
2021-03-02 05:45:59 +0100

    quicksort developing

--- datatypes/pointers/dynamic memory.c
+++ datatypes/pointers/dynamic memory.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef int feld[10];
+typedef int feld[10]; // feld is now a datatype datatype and defined as an array holding 10 integer values
 
-typedef feld* pointer_to_feld; // pointer_to_feld is a pointer to the first memory cell holding a feld
+typedef feld* pointer_to_feld; // pointer_to_feld is now a datatype and defined as a pointer to the first memory cell holding a feld
 
-pointer_to_feld a;
+pointer_to_feld a; //declared variable a as of type pointer_to_feld
 
 void pause()
 {
@@ -20,7 +20,7 @@ void pause()
 
 int main()
 {
-    a = (pointer_to_feld) malloc(sizeof(feld));
+    a = (pointer_to_feld) malloc(sizeof(feld)); // malloc(<int>) reserves the amount of bits/bites (not sure but i think bytes) given as int and returns the leading memory address of the reserved memory block, so basically a pointer
     for (int i = 0; i < 10; i++) { (*a)[i] = i+1; }
     for (int i = 0; i < 10; i++) { printf("\n%i", (*a)[i]); }
     pause();
