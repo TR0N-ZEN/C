f1ec0cbc2b805f95c4d32dab4aa9d0cb5e8028f1
Theo Reichert <theo.reichert7@gmail.com>
2022-03-26 16:45:18 +0100

    reorder and forking

---
+++ io/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+  char as[30]; // as is an array of ten characters
+  printf("Hello there.\n"); // write to the standard-output of the process
+  scanf("%s", as); // read from the standard-input of the process
+  printf(as); // write the content of the variable to the standard-output of the process
+  return 0;
+}
+
+// if you typed a string containing whitespace(s) then only the first word will be caught by scanf
+
